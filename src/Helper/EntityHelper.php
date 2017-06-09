bf556badc1d8efc8b5fa7da4a69f0d50f4b26031
Avram Cosmin <info@sprindo.co.uk>
2017-06-09 10:13:50 +0100

    Added Entity Query Builder Trait to the Entity Helper

--- src/Helper/EntityHelper.php
+++ src/Helper/EntityHelper.php
@@ -2,9 +2,12 @@
 
 namespace Mindlahus\SymfonyAssets\Helper;
 
+use Mindlahus\SymfonyAssets\Traits\Entity\EntityQueryBuilderTrait;
 use Mindlahus\SymfonyAssets\Traits\Entity\EntityTrait;
 
 class EntityHelper
 {
     use EntityTrait;
+
+    use EntityQueryBuilderTrait;
 }
---
+++ src/Traits/Entity/EntityQueryBuilderTrait.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Mindlahus\SymfonyAssets\Traits\Entity;
+
+trait EntityQueryBuilderTrait
+{
+    /**
+     * @param array $cols
+     * @param string $entityAndAlias
+     * @return string
+     */
+    static public function select(array $cols, string $entityAndAlias): string
+    {
+        return 'SELECT' . implode(', ', $cols) . ' FROM ' . $entityAndAlias;
+    }
+
+    /**
+     * @param array $entitiesAndTheirAliases
+     * @param string $joiningStrategy
+     * @return string
+     */
+    static public function join(array $entitiesAndTheirAliases, string $joiningStrategy = 'LEFT'): string
+    {
+        return implode(' ' . $joiningStrategy, $entitiesAndTheirAliases);
+    }
+}
