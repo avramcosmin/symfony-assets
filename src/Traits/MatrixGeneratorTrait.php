0d7ba45c305fb1a668a0beafba758b5a1e3d0409
Avram Cosmin <info@sprindo.co.uk>
2017-10-23 15:54:58 +0300

    Improved AB Matrix

--- src/Traits/MatrixGeneratorTrait.php
+++ src/Traits/MatrixGeneratorTrait.php
@@ -11,60 +11,107 @@ trait MatrixGeneratorTrait
      * @param string $colA
      * @param string $colB
      * @param string $colTotal
-     * @param bool $jsonEncode
+     * @param array $aHeaders
+     * @param array $bHeaders
      * @return array
      */
     public static function generateABMatrix(array $results,
                                             string $colA,
                                             string $colB,
                                             string $colTotal,
-                                            bool $jsonEncode = false): array
+                                            array $aHeaders,
+                                            array $bHeaders): array
     {
         $r = [
             'map' => [
                 'ab' => [],
-                'ba' => []
+                'ba' => [],
+                'aHeaders' => $aHeaders,
+                'bHeaders' => $bHeaders
             ],
-            'value' => [],
-            'sum' => [],
+            'value' => [
+                'n0' => [],
+                'p0' => []
+            ],
+            'sum' => [
+                'n0' => [],
+                'p0' => []
+            ],
             'total' => 0
         ];
         $glue = '_';
 
         foreach ($results as $result) {
-            $key = $glue . CryptoHelper::crc32Hash(
-                    bin2hex(random_bytes(20))
-                );
             $aKey = $glue . CryptoHelper::crc32Hash(
                     'a' . $glue . $result[$colA]
                 );
             $bKey = $glue . CryptoHelper::crc32Hash(
                     'b' . $glue . $result[$colB]
                 );
+            $key = $glue . CryptoHelper::crc32Hash(
+                    $aKey . $bKey
+                );
 
-            if (!$r['map']['ab'] ?? true) {
-                $r['map']['ab'][$aKey] = [
-                    'title' => $result[$colA],
-                    'key' => $aKey
-                ];
-            }
-            $r['map']['ab'][$aKey]['b'][] = $key;
-
-            if (!$r['map']['ba'] ?? true) {
-                $r['map']['ba'][$bKey] = [
-                    'title' => $result[$colB],
-                    'key' => $bKey
-                ];
-            }
-            $r['map']['ba'][$aKey]['a'][] = $key;
-
-            $r['value'][$key] = $result[$colTotal];
-
-            $r['sum'][$aKey] = ($r['sum'][$aKey] ?? 0) + (float)$colTotal;
-            $r['sum'][$bKey] = ($r['sum'][$bKey] ?? 0) + (float)$colTotal;
-            $r['total'] = ($r['total'] ?? 0) + (float)$colTotal;
+            $r['value']['n0'][$key] = $result[$colTotal];
         }
 
-        return $jsonEncode === true ? json_encode($r) : $r;
+        foreach ($aHeaders as $aHeader) {
+            $aKey = $glue . CryptoHelper::crc32Hash(
+                    'a' . $glue . $aHeader
+                );
+            foreach ($bHeaders as $bHeader) {
+                $bKey = $glue . CryptoHelper::crc32Hash(
+                        'b' . $glue . $bHeader
+                    );
+                $key = $glue . CryptoHelper::crc32Hash(
+                        $aKey . $bKey
+                    );
+
+                if (($r['map']['ab'][$aKey] ?? false) === false) {
+                    $r['map']['ab'][$aKey] = [
+                        'title' => $aHeader,
+                        'key' => $aKey
+                    ];
+                }
+                $r['map']['ab'][$aKey]['valueKeys'][] = $key;
+
+                if (($r['map']['ba'][$bKey] ?? false) === false) {
+                    $r['map']['ba'][$bKey] = [
+                        'title' => $bHeader,
+                        'key' => $bKey
+                    ];
+                }
+                $r['map']['ba'][$bKey]['valueKeys'][] = $key;
+
+                if (($r['value']['n0'][$key] ?? false) === false) {
+                    $r['value']['n0'][$key] = null;
+                } else {
+                    $r['sum']['n0'][$aKey] = ($r['sum']['n0'][$aKey] ?? 0) + (float)$r['value']['n0'][$key];
+                    $r['sum']['n0'][$bKey] = ($r['sum']['n0'][$bKey] ?? 0) + (float)$r['value']['n0'][$key];
+                }
+                $r['value']['p0'][$key] = null;
+            }
+        }
+        $r['total'] = array_sum(array_values($r['value']['n0']));
+
+        $r['map']['ab'] = array_values($r['map']['ab']);
+        $r['map']['ba'] = array_values($r['map']['ba']);
+
+        foreach ((array)$r['sum']['n0'] as $key => $value) {
+            $r['sum']['p0'][$key] = (string)round($value * 100 / $r['total'], 2);
+        }
+
+        foreach ((array)$r['map']['ab'] as $value) {
+            foreach ((array)$value['valueKeys'] as $valueKey) {
+                if ($r['value']['n0'][$valueKey] ?? null) {
+                    $r['value']['p0'][$valueKey] = (string)round(
+                        $r['value']['n0'][$valueKey] * 100 / $r['total'],
+                        2
+                    );
+                }
+            }
+        }
+
+        return $r;
     }
 }
